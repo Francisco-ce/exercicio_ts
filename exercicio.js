c5d786cda534955fbff36eff895802a8d99a1bdf
Francisco Prudente Correa <franciscopcorrea@gmail.com>
2024-08-11 11:25:27 -0300

    finalizando exercicio de typeScript

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiplicacao(8, 2));
+function recebeNome(nome) {
+    return `Olá ${nome}`;
+}
+console.log(recebeNome('Francisco'));
---
+++ exercicio.ts
@@ -0,0 +1,12 @@
+function multiplicacao(num1:number, num2:number):number {
+    return num1*num2
+}
+
+console.log(multiplicacao(8,2))
+
+
+function recebeNome(nome:string):string {
+    return `Olá ${nome}`
+}
+
+console.log(recebeNome('Francisco'))
